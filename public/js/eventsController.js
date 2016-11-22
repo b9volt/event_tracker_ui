02d27e4df18518c33b123f9f11d19aaf9abebdef
Brad Volts <b9volt@aol.com>
2016-11-21 17:02:43 -0800

    Adding forms

--- public/js/eventsController.js
+++ public/js/eventsController.js
@@ -6,13 +6,16 @@
   function eventsController($http){
 
     var self = this;
-    var server = "<YOUR_HEROKU_API_LINK_HERE>"
+    var server = "http://localhost:3000/"
     // For example, var server = 'https://enigmatic-garden-65625.herokuapp.com/api/'
 
     $http.get(`${server}/events`)
       .then(function(response) {
           self.events = response.data;
+          console.log(response.data);
           console.log(self.events[0].name);
     });
+
+
   }
 })()
